dc1d1ece309cb57bb5823ddea98273ae2ed55205
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-04-08 15:52:46 +0100

    Experimenting with date reformat

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,7 @@
 from flask import render_template, request
 
+from datetime import date
+
 from app import app
 from models.event import Event
 from models.event_list import events, add_event
@@ -10,11 +12,13 @@ def index():
 
 @app.route("/events", methods=["POST"])
 def create_event():
-    event_date = request.form["date"]
+    event_year = int(request.form["date"][0:4])
+    event_month = int(request.form["date"][6:7])
+    event_day = int(request.form["date"][9:10])
     event_name = request.form["name"]
     event_guests = request.form["num_guests"]
     event_room = request.form["room"]
     event_desc = request.form["description"]
-    new_event = Event(date = event_date, name = event_name, num_guests = event_guests, room = event_room, description = event_desc)
+    new_event = Event(date = date(event_year, event_month, event_day), name = event_name, num_guests = event_guests, room = event_room, description = event_desc)
     add_event(new_event)
     return render_template("index.html", title="Home", all_events=events)
--- models/event_list.py
+++ models/event_list.py
@@ -1,6 +1,8 @@
+from datetime import date
+
 from models.event import Event
 
-event1 = Event("someDate", "eventName", 40, "roomLoc", "eventDesc")
+event1 = Event(date(2022, 4, 9), "Farset Labs turns 10", 30, "Main event space", "Farset Labs opened its doors on 6th April 2012. Let's celebrate our birthday!")
 events = [event1]
 
 def add_event(event):
