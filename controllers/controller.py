5c084c24e463e021dceecf886c9568bb5ef6d5eb
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-04-08 16:52:12 +0100

    Redirect added

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,10 +1,14 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 
 from datetime import date
 
 from app import app
 from models.event import Event
 from models.event_list import events, add_event
+
+@app.route("/")
+def homepage_redir():
+    return redirect("/events")
 
 @app.route("/events")
 def index():
@@ -15,10 +19,11 @@ def create_event():
     event_year = int(request.form["date"][0:4])
     event_month = int(request.form["date"][6:7])
     event_day = int(request.form["date"][9:10])
+    event_date = date(event_year, event_month, event_day)
     event_name = request.form["name"]
     event_guests = request.form["num_guests"]
     event_room = request.form["room"]
     event_desc = request.form["description"]
-    new_event = Event(date = date(event_year, event_month, event_day), name = event_name, num_guests = event_guests, room = event_room, description = event_desc)
+    new_event = Event(date = event_date, name = event_name, num_guests = event_guests, room = event_room, description = event_desc)
     add_event(new_event)
     return render_template("index.html", title="Home", all_events=events)
--- models/event_list.py
+++ models/event_list.py
@@ -2,8 +2,12 @@ from datetime import date
 
 from models.event import Event
 
-event1 = Event(date(2022, 4, 9), "Farset Labs turns 10", 30, "Main event space", "Farset Labs opened its doors on 6th April 2012. Let's celebrate our birthday!")
-events = [event1]
+event1_desc = "Farset Labs opened its doors on 6th April 2012. Let's celebrate our birthday!"
+event2_desc = "Free for Farset Labs members, £3 suggested donation for non-members\n\nJoin us on the 23rd April 2022 for the first Vintage Computing Meetup at Farset Labs. Bring your own vintage computer or simply join in using one of the many on display. Make sure to register for the game competition (for bragging rights only) or simply talk to likeminded folk about your next vintage upgrade. You could even bring your hardware and upgrade it on the day! Commodore, Atari, classic IBM compatible or any vintage 80s/90s computer or games console."
+
+event1 = Event(date(2022, 4, 9), "Farset's 10th Birthday", 30, "Main event space", event1_desc)
+event2 = Event(date(2022, 4, 23), "Vintage Computing Meetup", 10, "Main event space", event2_desc)
+events = [event1, event2]
 
 def add_event(event):
     events.append(event)
