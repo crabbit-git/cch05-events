f458a6ca4916cca0d68be285af55a5c920399363
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-04-08 10:45:55 +0100

    Basic structure built

---
+++ controllers/controller.py
@@ -0,0 +1,9 @@
+from flask import render_template
+
+from app import app
+from models.event import Event
+from models.event_list import events
+
+@app.route("/events")
+def index():
+    return render_template("index.html", title="All Events", all_events=events)
---
+++ models/event_list.py
@@ -0,0 +1,4 @@
+from models.event import Event
+
+event1 = Event("someDate", "eventName", 40, "roomLoc", "eventDesc")
+events = [event1]
