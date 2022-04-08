9d8e6ba99ed1984032985eecf7f407c73a6409f6
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-04-08 11:37:13 +0100

    Basic webform added

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,9 +1,20 @@
-from flask import render_template
+from flask import render_template, request
 
 from app import app
 from models.event import Event
-from models.event_list import events
+from models.event_list import events, add_event
 
 @app.route("/events")
 def index():
     return render_template("index.html", title="All Events", all_events=events)
+
+@app.route("/events", methods=["POST"])
+def create_event():
+    event_date = request.form["date"]
+    event_name = request.form["name"]
+    event_guests = request.form["num_guests"]
+    event_room = request.form["room"]
+    event_desc = request.form["description"]
+    new_event = Event(date = event_date, name = event_name, num_guests = event_guests, room = event_room, description = event_desc)
+    add_event(new_event)
+    return render_template("index.html", title="Home", all_events=events)
--- models/event_list.py
+++ models/event_list.py
@@ -2,3 +2,6 @@ from models.event import Event
 
 event1 = Event("someDate", "eventName", 40, "roomLoc", "eventDesc")
 events = [event1]
+
+def add_event(event):
+    events.append(event)
